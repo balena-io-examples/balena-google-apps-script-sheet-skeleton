d5bebc095f11ae580b617d16dfde817cecae0306
AlidaOdendaal <alida@balena.io>
2020-05-29 20:43:36 +0200

    Fix to ensure scripts in ./dist/ are found and pushed
    Change-type: patch
    Signed-off-by: AlidaOdendaal <alida@balena.io>

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const GasPlugin = require('gas-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const src = path.resolve(__dirname, 'src');
+const destination = path.resolve(__dirname, 'dist');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.ts',
+  entry: `${src}/index.ts`,
   devtool: false,
   output: {
     filename: 'index.bundle.js',
@@ -20,5 +24,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts'],
   },
-  plugins: [new GasPlugin()],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${src}/../appsscript.json`, to: destination },
+      ]
+    }),
+    new GasPlugin(),
+  ],
 };
